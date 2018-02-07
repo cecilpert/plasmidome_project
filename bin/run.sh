7382b9b3314184f0ff5c62e664a5062a99b71088
chilpert2 <cecile.hilpert@gmail.com>
2018-02-07 16:34:16 +0100

    complete run.sh, add blast_treatment

--- bin/run.sh
+++ bin/run.sh
@@ -1,8 +1,34 @@
 set -e 
 
-results_dir=~/plasmidome_project/results/complete 
+results_dir=$1 
+db_dir=$2
+mode=$3
 
-./simulate_reads.sh $results_dir ~/plasmidome_project/plasmid_db/db148 complete
+megahit_assembly=$results_dir/assembly/megahit-am0.1 
+suffix=am0.1
+blast_prefix=$results_dir/assembly_evaluation/megahit_illumina_am0.1 
 
-megahit --12 $results_dir/grinder_simulated_illumina-reads.fasta -t 6 -o $results_dir/megahit_assembly
+if [ $mode == "test" ] 
+then 
+   db_prefix=$db_dir/plasmid148_test
+else 
+   db_prefix=$db_dir/plasmid148
+fi  
 
+./bin/simulate_reads.sh $results_dir $db_dir $suffix $mode
+
+mkdir -p $results_dir/assembly
+
+megahit --12 $results_dir/simulated_reads/grinder-illumina-20x-$suffix\-reads.fastq -t 6 -o $megahit_assembly
+
+
+mkdir -p $results_dir/assembly_evaluation 
+
+if [ ! -f $db_prefix.fasta.nhr ] 
+then 
+   makeblastdb -in $db_prefix.fasta -dbtype nucl 
+fi 
+
+blastn -query $megahit_assembly/final.contigs.fa -db $db_prefix.fasta -outfmt "6 qseqid sseqid pident qlen slen length mismatch gapope evalue bitscore" > $blast_prefix.blast.tsv
+
+python3 bin/treat_blast.py $blast_prefix.blast.tsv $blast_prefix.treatblast.tsv $blast_prefix.treatblast.interest.tsv 95 90 
--- bin/simulate_reads.sh
+++ bin/simulate_reads.sh
@@ -2,21 +2,24 @@ set -e
 
 outdir=$1
 db_dir=$2
-mode=$3 
+suffix=$3
+mode=$4
 
-mkdir -p $outdir
+mkdir -p $outdir/simulated_reads
+res_dir=$outdir/simulated_reads
 
 
-if [ "$mode" == "test" ] 
+if [ $mode == "test" ] 
 then
         echo "test" 
 	ref_file=$db_dir/plasmid148_test.fasta  
 else
+        echo "no test" 
 	ref_file=$db_dir/plasmid148.fasta  
 fi 	
 
 #generate Illumina reads, with no errors and no chimeras 
-grinder -rf $ref_file -cf 20 -rd 150 -id 350 -fq 1 -ql 30 10 -od $outdir -bn grinder_simulated_illumina  
+grinder -rf $ref_file -cf 20 -rd 150 -id 350 -fq 1 -ql 30 10 -od $res_dir -bn grinder-illumina-20x-$suffix -am powerlaw 0.1 
 
 #generate PacBio reads, with no errors and no chimeras, 5x coverage 
-grinder -rf $ref_file -cf 5 -rd 6000 -fq 1 -ql 30 10 -od $outdir -bn grinder_simulated_pacbio  
+grinder -rf $ref_file -cf 5 -rd 6000 -fq 1 -ql 30 10 -od $res_dir -bn grinder-pacbio-5x-$suffix -am powerlaw 0.1  
