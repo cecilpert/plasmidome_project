c7c607d32b69c805396f16969b5bf6171dc0ba03
chilpert2 <cecile.hilpert@gmail.com>
2018-02-07 12:21:38 +0100

    add creation of stats file for plasmids db

--- pre_treatment/plasmid_148_dw.py
+++ pre_treatment/plasmid_148_dw.py
@@ -4,21 +4,27 @@ import sys
 
 list_acc=sys.argv[1]
 out_fasta=sys.argv[2]
-out_fasta_dir=sys.argv[3]
+#out_fasta_dir=sys.argv[3]
+out_stats=sys.argv[3]
 
 Entrez.email = "cecile.hilpert@gmail.com"
 
-list_acc_f=open(list_acc,"r") 
+list_acc_f=open(list_acc,"r")
+out_stats_f=open(out_stats,"w")
+out_stats_f.write("id\tdesc\tlength\n")  
 list_records=[]
 for l in list_acc_f: 
-   print(l)
    handle = Entrez.efetch(db="nucleotide",id=l.rstrip(),rettype="fasta",retmode="text")
-   list_records.append(SeqIO.read(handle,"fasta"))
+   rec=SeqIO.read(handle,"fasta")
+   out_stats_f.write(rec.id+"\t"+rec.description+"\t"+str(len(rec.seq))+"\n") 
+   list_records.append(rec)
    handle.close()
     
 with open(out_fasta,"w") as output_handle : 
    SeqIO.write(list_records,output_handle,"fasta") 
 
-for record in list_records : 
-   with(open(out_fasta_dir+'/'+record.id+".fasta",'w')) as output_handle: 
-      SeqIO.write(record,output_handle,"fasta") 
+#for record in list_records : 
+#   with(open(out_fasta_dir+'/'+record.id+".fasta",'w')) as output_handle: 
+#      SeqIO.write(record,output_handle,"fasta") 
+
+out_stats_f.close() 
