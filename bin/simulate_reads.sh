f1ce373f52997047b8e23658e2fa8242b72eac15
chilpert2 <cecile.hilpert@gmail.com>
2018-02-13 12:12:52 +0100

    simulate reads 20X Illumina and 5X Pacbio, just plasmids

--- bin/simulate_reads.sh
+++ bin/simulate_reads.sh
@@ -1,25 +1,32 @@
 set -e 
 
+function usage(){
+	echo 'usage : run_assembly_evaluation.sh <outdir> <reference file> <suffix simulated reads>' 
+}
+
+if [ "$#" -ne 3 ]
+then 
+	usage 
+	exit 1 
+fi
+
+
 outdir=$1
-db_dir=$2
+ref_file=$2
 suffix=$3
-mode=$4
-
-mkdir -p $outdir/simulated_reads
-res_dir=$outdir/simulated_reads
 
 
-if [ $mode == "test" ] 
-then
-        echo "test" 
-	ref_file=$db_dir/plasmid148_test.fasta  
-else
-        echo "no test" 
-	ref_file=$db_dir/plasmid148.fasta  
-fi 	
+mkdir -p $outdir
+
+
+ 	
 
 #generate Illumina reads, with no errors and no chimeras 
-grinder -rf $ref_file -cf 20 -rd 150 -id 350 -fq 1 -ql 30 10 -od $res_dir -bn grinder-illumina-$suffix -am powerlaw 0.1 
+grinder -rf $ref_file -cf 20 -rd 150 -id 350 -fq 1 -ql 30 10 -od $outdir -bn grinder-illumina-$suffix -am powerlaw 0.1 
+
+#Generate abundance file for Pacbio
+cut -f 2,3 $outdir/grinder-illumina-$suffix-ranks.txt | tail -n +2 > $outdir/grinder-illumina-$suffix-af.txt
+af=$outdir/grinder-illumina-$suffix-af.txt
 
 #generate PacBio reads, with no errors and no chimeras, 5x coverage 
-grinder -rf $ref_file -cf 5 -rd 6000 -fq 1 -ql 30 10 -od $res_dir -bn grinder-pacbio-5x-$suffix -am powerlaw 0.1  
+grinder -rf $ref_file -cf 5 -rd 6000 -fq 1 -ql 30 10 -od $outdir -bn grinder-pacbio-5x-$suffix -am powerlaw 0.1 -af $af  
