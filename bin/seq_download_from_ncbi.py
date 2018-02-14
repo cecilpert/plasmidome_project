e3f6fb63e2ddfc568ec4aa75269af53568455a9f
chilpert2 <cecile.hilpert@gmail.com>
2018-02-14 17:18:42 +0100

    Other modifications

--- bin/seq_download_from_ncbi.py
+++ bin/seq_download_from_ncbi.py
@@ -2,6 +2,13 @@ from Bio import Entrez
 from Bio import SeqIO
 import sys 
 
+def usage(): 
+	print("usage : python3 seq_download_from_ncbi.py <file_with_accession_numbers> <out_fasta_file> <out_stats_file>") 
+	
+if len(sys.argv)!=4 : 
+	usage()
+	exit() 
+
 list_acc=sys.argv[1]
 out_fasta=sys.argv[2]
 #out_fasta_dir=sys.argv[3]
@@ -14,6 +21,7 @@ out_stats_f=open(out_stats,"w")
 out_stats_f.write("id\tdesc\tlength\n")  
 list_records=[]
 for l in list_acc_f: 
+   print(l)
    handle = Entrez.efetch(db="nucleotide",id=l.rstrip(),rettype="fasta",retmode="text")
    rec=SeqIO.read(handle,"fasta")
    out_stats_f.write(rec.id+"\t"+rec.description+"\t"+str(len(rec.seq))+"\n") 
@@ -22,9 +30,5 @@ for l in list_acc_f:
     
 with open(out_fasta,"w") as output_handle : 
    SeqIO.write(list_records,output_handle,"fasta") 
-
-#for record in list_records : 
-#   with(open(out_fasta_dir+'/'+record.id+".fasta",'w')) as output_handle: 
-#      SeqIO.write(record,output_handle,"fasta") 
 
 out_stats_f.close() 
