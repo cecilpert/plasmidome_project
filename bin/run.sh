f1e1564337756b08df966d0dfc82e51e0cd1bc04
chilpert2 <cecile.hilpert@gmail.com>
2018-02-14 17:08:05 +0100

    rewrite run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,42 +1,18 @@
 set -e 
 
-results_dir=$1 
-db_dir=$2
-mode=$3
+outdir=$1 
+db_dir=$2 
 
-megahit_assembly=$results_dir/assembly/megahit-20x-am0.1 
-spades_assembly=$results_dir/assembly/metaspades-20x-am0.1
-suffix=20x-am0.1 
-blast_prefix=align_contig_plasmids
-eval_megahit=$results_dir/assembly_evaluation/megahit-20x-am0.1
-eval_metaspades=$results_dir/assembly_evaluation/metaspades-20x-am0.1
+mkdir -p $outdir 
 
-if [ $mode == "test" ] 
-then 
-   db_prefix=$db_dir/plasmid148_test
-else 
-   db_prefix=$db_dir/plasmid148_no_dupl
-fi  
+python3 bin/separate_plasmids_chrm.py $db_dir/complete_genomes $db_dir complete
 
-#./bin/simulate_reads.sh $results_dir $db_dir $suffix $mode
+bash bin/simulate_reads.sh $outdir/simulate_reads $db_dir/plasmids.fasta 20X-am0.1 $db_dir/summary_plasmids.tsv $db_dir/summary_chrm.tsv $db_dir/bacteria_chrm.fasta 
 
-mkdir -p $results_dir/assembly
+bash bin/run_assembly.sh --megahit --metaspades --hybridspades -o $outdir/assembly -i $outdir/simulated_reads/grinder-illumina-20X-am0.1-reads.fastq -l $outdir/simulated_reads/grinder-pacbio-5x-20X-am0.1-reads.fastq 
 
-#megahit --12 $results_dir/simulated_reads/grinder-illumina-$suffix\-reads.fastq -t 6 -o $megahit_assembly
-#/usr/local/SPAdes-3.9.0-Linux/bin/spades.py --12 $results_dir/simulated_reads/grinder-illumina-$suffix\-reads.fastq -t 6 --meta --only-assembler -o $spades_assembly   
+mkdir -p $outdir/assembly_evaluation
+metaquast.py -R $db_dir/plasmid_sequences -l "megahit,metaspades,hybridspades" -o $outdir/assembly_evaluation/metaquast $outdir/assembly/megahit/final.contigs.fa $outdir/assembly/metaspades/scaffolds.fasta $outdir/assembly/hybridspades/scaffolds.fasta 
 
-mkdir -p $results_dir/assembly_evaluation 
 
-if [ ! -f $db_prefix.fasta.nhr ] 
-then 
-   makeblastdb -in $db_prefix.fasta -dbtype nucl 
-fi 
- 
-mkdir -p $eval_megahit
-mkdir -p $eval_metaspades 
- 
-blastn -query $megahit_assembly/final.contigs.fa -db $db_prefix.fasta -outfmt "6 qseqid sseqid pident qlen slen length mismatch gapope evalue bitscore" > $eval_megahit/$blast_prefix.blast.tsv
-blastn -query $spades_assembly/scaffolds.fasta -db $db_prefix.fasta -outfmt "6 qseqid sseqid pident qlen slen length mismatch gapope evalue bitscore" > $eval_metaspades/$blast_prefix.blast.tsv 
-
-python3 bin/treat_blast.py $eval_megahit/$blast_prefix.blast.tsv $eval_megahit/$blast_prefix.treatblast.tsv $eval_megahit/$blast_prefix.treatblast.interest.tsv 95 90
-python3 bin/treat_blast.py $eval_metaspades/$blast_prefix.blast.tsv $eval_metaspades/$blast_prefix.treatblast.tsv $eval_metaspades/$blast_prefix.treatblast.interest.tsv 95 90 
+  
